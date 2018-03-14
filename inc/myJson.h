8107b3524ca35e33a16f85c9710ae2d7250a2a73
Dream-Lantern <aeiou54@163.com>
2018-03-14 11:14:07 +0800

    增加数组默认长度

--- inc/myJson.h
+++ inc/myJson.h
@@ -8,16 +8,16 @@ class myJson
         int readJson_Int(const std::string & strValue, std::string key, int &value);
         // 解析 string 型 value
         int readJson_String(const std::string & strValue, std::string key, std::string &value);
-        // 解析 数组中的 int
-        int readJson_ArrayObj_Int(const std::string & strValue, std::string obj, std::string key, int &value);
-        // 解析 数组中的 double
-        int readJson_ArrayObj_Double(const std::string & strValue, std::string obj, std::string key, double &value);
-        // 解析 数组中的 string
-        int readJson_ArrayObj_String(const std::string & strValue, std::string obj, std::string key, std::string &value);
-        // 解析 数组中的 bool
-        int readJson_ArrayObj_Bool(const std::string & strValue, std::string obj, std::string key, bool &value);
-        // 解析 数组中的 obj
-        int readJson_ArrayObj_Obj(const std::string & strValue, std::string obj, std::string key, std::string keyObj, Json::Value &value);
+        // 解析 数组中的 int  最后一个参数为默认解析数组的长度, 
+        int readJson_ArrayObj_Int(const std::string & strValue, std::string obj, std::string key, int &value, int len = 1);
+        // 解析 数组中的 double 最后一个参数为默认解析数组的长度 
+        int readJson_ArrayObj_Double(const std::string & strValue, std::string obj, std::string key, double &value, int len = 1);
+        // 解析 数组中的 string 最后一个参数为默认解析数组的长度 
+        int readJson_ArrayObj_String(const std::string & strValue, std::string obj, std::string key, std::string &value, int len = 1);
+        // 解析 数组中的 bool 最后一个参数为默认解析数组的长度 
+        int readJson_ArrayObj_Bool(const std::string & strValue, std::string obj, std::string key, bool &value, int len = 1);
+        // 解析 数组中的 obj 最后一个参数为默认解析数组的长度 
+        int readJson_ArrayObj_Obj(const std::string & strValue, std::string obj, std::string key, std::string keyObj, Json::Value &value, int len = 1);
 
         // 获取 单例方法
         static myJson* getInstance();
