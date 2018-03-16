b6e477af4b29f3013e0861f5728bae95e72b4d2a
Dream-Lantern <aeiou54@163.com>
2018-03-17 01:12:05 +0800

    简化main流程

--- src/Face.cpp
+++ src/Face.cpp
@@ -3,6 +3,7 @@
 #include <strings.h>
 #include <curl/curl.h>
 #include <json/json.h>
+#include <stdio.h>
 #include "base64.h"
 #include "fileOP.h"
 #include "getAccessToken.h"
@@ -14,6 +15,7 @@
 #include "animalDetect.h"
 #include "botanyDetect.h"
 #include "fdfsUploadFile.h"
+#include "mySQL.h"
 
 using namespace std;
 
@@ -27,8 +29,8 @@ int main(int argc, char *argv[])
     string accessToken;
     getAccessToken *access = getAccessToken::getInstance();
     access->get_access_token(accessToken, getAccessToken::API_Key, getAccessToken::Secure_Key);
-     
-    //face
+
+    // new 一个业务对象 face
     absBusiness* face = new faceDetect;
     int ret = face->discern(accessToken, fileByte);
     if (ret != 0)
@@ -37,24 +39,29 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    Json::Value res = face->resJson();
-    cout << res << endl;
-
     // 上传 图片到 fdfs,  fileId 为 fdfs里存储的文件名称
-    char fileId[64] = {0};
-    uploadFile* upFile = uploadFile::getInstance();
-    // 此时 argv[1] 为 php保存到本地的 绝对路径
-    string fileNameTmp = argv[1];
-
-    const char* fileName = fileNameTmp.c_str();
-    ret = upFile->myUploadFile(upFile->m_confFile, fileName, fileId, sizeof(fileId));
+    ret = face->uploadFdfs(argv[1]);
     if (ret != 0)
     {
-        cout << "fdfs err" << endl;
         delete(face);
         return -1;
     }
-    
+
+    // TODO 此处应当从配置文件中读取 IP等信息
+   
+    // 将图片url, json数据存储到 mysql
+    const char* localhost = "localhost";
+    const char* user = "root";
+    const char* pswd = "qwe123";
+    const char* dbName = "discern_img";
+    ret = face->saveDB(localhost, user, pswd, dbName);
+    if (ret != 0)
+    {
+        cout << "insert err" << endl;
+        return -1;
+    }
+    cout << "insert succ" << endl;
     delete(face);
-	return 0;
+
+    return 0;
 }
