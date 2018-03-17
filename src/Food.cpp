73c88dd88f2a1e341b4e78b868cc19bee00b0e12
Dream-Lantern <aeiou54@163.com>
2018-03-18 01:15:21 +0800

    优化main函数

--- src/Food.cpp
+++ src/Food.cpp
@@ -3,6 +3,7 @@
 #include <strings.h>
 #include <curl/curl.h>
 #include <json/json.h>
+#include <stdio.h>
 #include "base64.h"
 #include "fileOP.h"
 #include "getAccessToken.h"
@@ -14,6 +15,7 @@
 #include "animalDetect.h"
 #include "botanyDetect.h"
 #include "fdfsUploadFile.h"
+#include "mySQL.h"
 
 using namespace std;
 
@@ -31,32 +33,37 @@ int main(int argc, char *argv[])
     //food
     absBusiness* food = new foodDetect;
     int ret = food->discern(accessToken, fileByte);
-
-
     if (ret != 0)
     {
         cout << "get res err!" << endl;
         return -1;
     }
 
-    Json::Value res = food->resJson();
-    cout << res << endl;
-
     // 上传 图片到 fdfs,  fileId 为 fdfs里存储的文件名称
-    char fileId[64] = {0};
-    uploadFile* upFile = uploadFile::getInstance();
-    // 此时 argv[1] 为 php保存到本地的 绝对路径
-    string fileNameTmp = argv[1];
-
-    const char* fileName = fileNameTmp.c_str();
-    ret = upFile->myUploadFile(upFile->m_confFile, fileName, fileId, sizeof(fileId));
+    ret = food->uploadFdfs(argv[1]);
     if (ret != 0)
     {
-        cout << "fdfs err" << endl;
         delete(food);
         return -1;
     }
-    
+
+    // TODO 此处应当从配置文件中读取 IP等信息
+   
+    // 将图片url, json数据存储到 mysql
+    const char* localhost = "localhost";
+    const char* user = "root";
+    const char* pswd = "qwe123";
+    const char* dbName = "discern_img";
+    ret = food->saveDB(localhost, user, pswd, dbName);
+    if (ret != 0)
+    {
+        //cout << "insert err" << endl;
+        //插入数据库失败
+        delete(food);
+        return -1;
+    }
+    //cout << "insert succ" << endl;
     delete(food);
+
 	return 0;
 }
