7e89af028187f80026711434ba1e7189e5c29de8
Dream-Lantern <aeiou54@163.com>
2018-03-18 17:29:31 +0800

    读取配置文件

--- inc/init.h
+++ inc/init.h
@@ -7,13 +7,22 @@ class Judge
     public:
     // 获取accessToken, 读取文件, 转换base64
     string init(char* fileName);
+    // 读取配置文件信息
+    int readConf(string path);
 
     // 存储二进制文件
     string m_uploadByte;
     char *m_fileByte;
     char *m_fileBase64;
+    // 存储 accessToken
+    string m_accessToken;
+    // 存储 数据库的信息
+    string m_host;
+    string m_user;
+    string m_passwd;
+    string m_dbName;
 
-    // 析构, 释放掉 读取文件的字节流
+    // 析构, 释放 读取文件的字节流
     ~Judge();
 
 };
