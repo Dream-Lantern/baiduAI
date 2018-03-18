d9d9b1c253276e012cae375ce6a114e523d0709b
Dream-Lantern <aeiou54@163.com>
2018-03-18 17:30:03 +0800

    优化main

--- src/Botany.cpp
+++ src/Botany.cpp
@@ -25,39 +25,14 @@ int main(int argc, char *argv[])
     Judge judge;
     string fileByte = judge.init(argv[1]);
 
-    // 获取 access
-    string accessToken;
-    getAccessToken *access = getAccessToken::getInstance();
-    access->get_access_token(accessToken, getAccessToken::API_Key, getAccessToken::Secure_Key);
-
-    // botany
+    // botany , 发送 post 请求给百度
     absBusiness* botany = new botanyDetect;
-    int ret = botany->discern(accessToken, fileByte);
+    int ret = botany->discern(judge.m_accessToken, fileByte);
     if (ret != 0)
     {
         cout << "get res err!" << endl;
         return -1;
     }
-
-    /*
-    Json::Value res = botany->resJson();
-    cout << res << endl;
-
-    // 上传 图片到 fdfs,  fileId 为 fdfs里存储的文件名称
-    char fileId[64] = {0};
-    uploadFile* upFile = uploadFile::getInstance();
-    // 此时 argv[1] 为 php保存到本地的 绝对路径
-    string fileNameTmp = argv[1];
-
-    const char* fileName = fileNameTmp.c_str();
-    ret = upFile->myUploadFile(upFile->m_confFile, fileName, fileId, sizeof(fileId));
-    if (ret != 0)
-    {
-        cout << "fdfs err" << endl;
-        delete(botany);
-        return -1;
-    }
-    */
     
     // 上传 图片到 fdfs,  fileId 为 fdfs里存储的文件名称
     ret = botany->uploadFdfs(argv[1]);
@@ -66,23 +41,15 @@ int main(int argc, char *argv[])
         delete(botany);
         return -1;
     }
-
-    // TODO 此处应当从配置文件中读取 IP等信息
    
     // 将图片url, json数据存储到 mysql
-    const char* localhost = "localhost";
-    const char* user = "root";
-    const char* pswd = "qwe123";
-    const char* dbName = "discern_img";
-    ret = botany->saveDB(localhost, user, pswd, dbName);
+    ret = botany->saveDB(judge.m_host.data(), judge.m_user.data(), judge.m_passwd.data(), judge.m_dbName.data());
     if (ret != 0)
     {
-        //cout << "insert err" << endl;
         // 插入数据库失败
         delete(botany);
         return -1;
     }
-    //cout << "insert succ" << endl;
     
     delete(botany);
 	return 0;
